5113752ec1a967cdadd075cd4ad8823b98fd6d44
MaissonSM <maissonsaraiva@gmail.com>
2025-01-15 11:14:45 -0300

    test: fix in test of web

---
+++ apps/web/test/test.spec.tsx
@@ -0,0 +1,9 @@
+import { describe, expect, it } from '@jest/globals';
+
+describe('Root layout', () => {
+  describe('metadata', () => {
+    it('should be exported', () => {
+      expect(true).toBeTruthy();
+    });
+  });
+});
